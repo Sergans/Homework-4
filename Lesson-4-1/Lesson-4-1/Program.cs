7355e5f6c5dfbcb32ffb2d5ca9b3a9e425441187
Сергей <gan7er@yandex.ru>
2021-02-27 15:15:14 +0300

    Конечный вариант

--- Lesson-4-1/Lesson-4-1/Program.cs
+++ Lesson-4-1/Lesson-4-1/Program.cs
@@ -10,6 +10,26 @@ namespace Lesson_4_1
     {
         static void Main(string[] args)
         {
+            //Вариант программы на 3 последовательности
+            string[] chel = new string[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Введите данные по инструкции: Имя,Фамилию,Отчество. Вы ввели {i}-(Всего 3)");
+                Console.WriteLine("Введите имя");
+                string ferstName = Console.ReadLine();
+                Console.WriteLine("Введите фамилию");
+                string lastName = Console.ReadLine();
+                Console.WriteLine("Введите отчество");
+                string patronymic = Console.ReadLine();
+                chel[i] = GetFullName(ferstName, lastName, patronymic);
+                Console.Clear();
+            }
+            Console.Clear();
+            Console.WriteLine("Выводим список ФИО");
+            foreach (string sps in chel)
+            {
+                Console.WriteLine($"ФИО:{sps}");
+            }
 
         }
         //метод
