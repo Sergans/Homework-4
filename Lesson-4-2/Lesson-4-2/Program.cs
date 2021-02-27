f0e900373366b2cca6f6059af91ecd4330945aab
Сергей <gan7er@yandex.ru>
2021-02-27 15:23:15 +0300

    Набросок программы

---
+++ Lesson-4-2/Lesson-4-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Набросок
+            Console.WriteLine("Введите последовательность целых чисел через пробел чтобы вывести сумму");
+            string nomber = Console.ReadLine();//Строка с пробелами
+            Console.WriteLine(SumStr(nomber));
+        }
+
+    }
+}
