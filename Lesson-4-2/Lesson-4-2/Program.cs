871993e496a4cbd66c49d8834e0f8fb1ef260663
Сергей <gan7er@yandex.ru>
2021-02-27 15:26:28 +0300

    С методом.Конечный вариант

--- Lesson-4-2/Lesson-4-2/Program.cs
+++ Lesson-4-2/Lesson-4-2/Program.cs
@@ -11,9 +11,22 @@ namespace Lesson_4_2
         static void Main(string[] args)
         {
             //Набросок
-            Console.WriteLine("Введите последовательность целых чисел через пробел чтобы вывести сумму");
+            Console.WriteLine("Введите последовательность целых чисел через пробел, чтобы вывести сумму");
             string nomber = Console.ReadLine();//Строка с пробелами
             Console.WriteLine(SumStr(nomber));
+        }
+        static int SumStr(string nomber)
+        {
+            int sum = 0;
+            string[] nmb = nomber.Split(' ');//разбиваем строку на массив строк
+            int[] arr = new int[nmb.Length];//Создаем массив инт
+            //Преобразуем стринг в инт и суммируем значения
+            for (int i = 0; i < nmb.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(nmb[i]);
+                sum += arr[i];
+            }
+            return sum;//Возвращаем сумму
         }
 
     }
