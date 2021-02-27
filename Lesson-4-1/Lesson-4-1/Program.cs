e344b207283cbbb8889b57415289d9f6f848796e
Сергей <gan7er@yandex.ru>
2021-02-27 15:09:09 +0300

    Пишем метод

---
+++ Lesson-4-1/Lesson-4-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        //метод
+        static string GetFullName(string ferstName, string lastName, string patronymic)
+        {
+            string a = ($"{lastName} {ferstName} {patronymic}");
+            return a;
+        }
+    }
+}
