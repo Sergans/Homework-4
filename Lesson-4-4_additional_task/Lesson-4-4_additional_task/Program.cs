3e696f6546aa3e25179bb606731077d86f3ac371
Сергей <gan7er@yandex.ru>
2021-02-27 15:44:07 +0300

    Набросок

---
+++ Lesson-4-4_additional_task/Lesson-4-4_additional_task/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_4_additional_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Набросок
+            Console.WriteLine("Введите значение");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] f = new int[n];
+
+            Console.WriteLine(Fibbo(n));
+        }
+    }
+}
