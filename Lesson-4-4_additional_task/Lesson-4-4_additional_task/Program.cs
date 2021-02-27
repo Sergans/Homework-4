b251f1c780d42604f65968ca546654483e4ec9d3
Сергей <gan7er@yandex.ru>
2021-02-27 15:46:05 +0300

    Метод

--- Lesson-4-4_additional_task/Lesson-4-4_additional_task/Program.cs
+++ Lesson-4-4_additional_task/Lesson-4-4_additional_task/Program.cs
@@ -17,5 +17,20 @@ namespace Lesson_4_4_additional_task
 
             Console.WriteLine(Fibbo(n));
         }
+        static int Fibbo(int n)
+        {
+            if (n == 0)
+            {
+                n = 0;
+                return n;
+            }
+            else if (n == 1)
+            {
+                n = 1;
+                return n;
+            }
+            return n = Fibbo(n - 1) + Fibbo(n - 2);
+
+        }
     }
 }
